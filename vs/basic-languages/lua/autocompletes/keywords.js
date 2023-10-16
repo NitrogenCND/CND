e07f256653ad34ce26dffbc16caeda6b372a5260
NitrogenCND <147336685+NitrogenCND@users.noreply.github.com>
2023-10-16 14:12:40 +0200

    Add files via upload

---
+++ vs/basic-languages/lua/autocompletes/keywords.js
@@ -0,0 +1,27 @@
+define(["require", "exports"], function (require, exports) {
+	Object.defineProperty(exports, "__esModule", { value: true });
+
+	exports.autocompletes = {
+		"Keyword": {
+
+			// Operators:
+			"and": {},
+			"not": {},
+			"or": {},
+
+			// Keywords:
+			"break": {},
+			"continue": {},
+			"end": {},
+			"in": {},
+			"local": {},
+			"then": {},
+			"until": {},
+			"Nitrogen": {},
+
+			// Special words:
+			"self": {},
+
+		},
+	};
+});
